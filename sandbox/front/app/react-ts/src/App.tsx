7067b0bae19772b3ad154fb0d5e1e2d97541d652
apayet <apayet@student.42.fr>
2023-07-10 15:36:17 +0200

    ADD REACT-ROUTER-DOM:
     On branch ap/ws_transmit_basic_data_front_html_js_and_nestjs
     Your branch is up to date with 'origin/ap/ws_transmit_basic_data_front_html_js_and_nestjs'.
    
     Changes to be committed:
    	modified:   sandbox/front/app/react-ts/package.json
    	modified:   sandbox/front/app/react-ts/src/App.tsx
    	modified:   sandbox/front/walkthrough

--- sandbox/front/app/react-ts/src/App.tsx
+++ sandbox/front/app/react-ts/src/App.tsx
@@ -1,8 +1,13 @@
 
 // https://mui.com/material-ui/customization/dark-mode/
+// eslint-disable-next-line max-len
+// https://dev.to/collins87mbathi/reactjs-protected-route-m3j#:~:text=To%20create%20a%20protected%20route,redirected%20to%20the%20login%20page. 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useAppSelector } from "./hooks/redux-hooks";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import ReduxViewTest from "./components/ReduxViewTest";
 
 const	darkTheme = createTheme(
@@ -21,6 +26,7 @@ const	lightTheme = createTheme(
 	}
 });
 
+
 const App = () =>
 {
 	let		usedTheme;
@@ -36,7 +42,13 @@ const App = () =>
 	return (
 		<ThemeProvider theme={usedTheme}>
 			<CssBaseline />
-			<ReduxViewTest />
+			<BrowserRouter >
+				<Routes>
+					<Route path="/" element={<h1>Home</h1>} />
+					<Route path="/redux-test-view" element={<ReduxViewTest/>} />
+					<Route path="*" element={<h1>Error 404</h1>} />
+				</Routes>
+			</BrowserRouter>
 		</ThemeProvider>
 	);
 };
