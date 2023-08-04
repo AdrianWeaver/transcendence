5b475f6ba93db34593a27f1f39ed685209abc83d
apayet <apayet@localhost.localdomain>
2023-08-04 12:35:09 +0200

    ADD ROUTER'S INTO APP.TSX:
    Router are created, the entrypoint of the router is MainRouter.tsx
    other :
    	LoggedRouter : for connected user
    	VisitorRouter : for people that not auth
    	Registration : for people are logged in.
    
    useRedirectRegistration customHook created

---
+++ front/app/src/Router/LoggedRouter.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable max-lines-per-function */
+import {
+	BrowserRouter,
+	Route,
+	Routes,
+} from "react-router-dom";
+
+/**
+ * This is the router for a logged user
+ * */
+const	LoggedRouter = () =>
+{
+	return (
+		<BrowserRouter >
+			<Routes>
+				{/* need to save route navigation */}
+				<Route
+					path="/"
+					element={<h1>home view</h1>}
+				/>
+				<Route
+					path="/redux-test-view"
+					element={<p>{"<ReduxViewTest>"}</p>}
+				/>
+
+				{/* no save route navigation */}
+				<Route
+					path="/logout"
+					element={<p>{"<Logout>"}</p>}
+				/>
+				<Route
+					path="*"
+					element={<h1>Error 404</h1>}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default	LoggedRouter;
---
+++ front/app/src/Views/Signup/Signup.tsx
@@ -0,0 +1,17 @@
+import
+{
+	useRedirectRegistration
+}	from "../../Router/Hooks/useRedirectRegistration";
+
+const	Signup = () =>
+{
+	useRedirectRegistration();
+
+	return (
+		<>
+			Signup
+		</>
+	);
+};
+
+export default Signup;
