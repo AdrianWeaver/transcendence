e1aca9a985b1215aea8f804292f5ef5aa22dfa18
Nikola BORATKOVA <nikol.boratkova@gmail.com>
2023-08-24 09:10:40 -0500

    ADD REMOVEEVENTLISTENER FUNCTION:
    Adding a function to stop listening to the events at the end of the
    game.

--- front/app/src/Views/GameCanvas/GameCanvas.tsx
+++ front/app/src/Views/GameCanvas/GameCanvas.tsx
@@ -126,6 +126,9 @@ const	GameCanvas = () =>
 		return (() =>
 		{
 			cancelAnimationFrame(requestId);
+			removeEventListener("keydown", keyHookDown);
+			removeEventListener("keyup", keyHookReleased);
+			removeEventListener("keydown", keyEnter);
 		});
 	});
 
