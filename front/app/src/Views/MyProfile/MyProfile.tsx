bbdfacfa8f1b838f85e258e010310f118b25ff97
apayet <apayet@localhost.localdomain>
2023-10-02 20:17:46 +0200

    ADD USER DATA EXTRACTED FROM API:
    This is user data from API,
    This is designed to start implement the view for the profile.

--- front/app/src/Views/MyProfile/MyProfile.tsx
+++ front/app/src/Views/MyProfile/MyProfile.tsx
@@ -2,9 +2,13 @@ import { useEffect } from "react";
 import MenuBar from "../../Component/MenuBar/MenuBar";
 import { useSavePrevPage } from "../../Router/Hooks/useSavePrevPage";
 
+import data from "./realFakeData.json";
+
 const	MyProfile = () =>
 {
 	const	savePrevPage = useSavePrevPage();
+
+	console.log(data);
 
 	useEffect(() =>
 	{
