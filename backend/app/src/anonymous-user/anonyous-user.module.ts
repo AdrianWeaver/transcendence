0160f1dedd2805c3186509831debbadccca81ba8
apayet <apayet@student.42.fr>
2023-10-06 17:31:27 +0200

    ADD CONTROLLER TO MODULES:
    Was not instanciated, now fixed

--- backend/app/src/anonymous-user/anonyous-user.module.ts
+++ backend/app/src/anonymous-user/anonyous-user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AnonymousUserService } from "./anonymous-user.service";
+import { AnonymousUserController } from "./anonymous-user.controller";
 
 
 @Module({
-	providers: [AnonymousUserService],
+	providers: [AnonymousUserService, AnonymousUserController],
 	exports: [AnonymousUserService]
 })
 export class AnonymousUserModule
