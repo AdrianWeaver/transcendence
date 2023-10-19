8510fb8d259e847bc38aa75cff1fa8dbd91769e8
jcourtoi <jcourtoi@student.42.fr>
2023-10-19 18:00:14 +0200

    ADD DOCKER COMPOSE FILE:
    Finally, he's here heheh

---
+++ backend/entry.sh
@@ -0,0 +1,13 @@
+# sleep 2 #very ugly
+set -e
+until pg_isready -h "${POSTGRES_HOST}" -p 5432 -U "${POSTGRES_USER}"; do
+  echo "$(date) - waiting for db to start"
+  sleep 1
+done
+exec "$@"
+# npx npx prisma migrate dev --name init
+npx prisma db push
+npx prisma generate
+npx prisma db seed
+(npx prisma studio&) 
+npm run start:dev
--- backend/script.sh
+++ backend/script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-npm install
+cd app && npm install
+cd src/prisma
+npx prisma generate
+npx npx prisma migrate dev --schema=./prisma/schema.prisma
+npx prisma db pull
+npm run start:dev
 
 exec "$@"
