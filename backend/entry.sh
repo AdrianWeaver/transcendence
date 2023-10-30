fb35d6a8b058e823050495f32bf442e71a64e565
apayet <apayet@student.42.fr>
2023-10-30 14:34:14 +0100

    FIX ISSUE WITH DB:
    Now the schema is recreated at each build

--- backend/entry.sh
+++ backend/entry.sh
@@ -1,13 +1,16 @@
-# sleep 2 #very ugly
-set -e
-until pg_isready -h "${POSTGRES_HOST}" -p 5432 -U "${POSTGRES_USER}"; do
-  echo "$(date) - waiting for db to start"
-  sleep 1
-done
-exec "$@"
-# npx npx prisma migrate dev --name init
-npx prisma db push
-npx prisma generate
-npx prisma db seed
-(npx prisma studio&) 
-npm run start:dev
+# # sleep 2 #very ugly
+# set -e
+# until pg_isready -h "${POSTGRES_HOST}" -p 5432 -U "${POSTGRES_USER}"; do
+#   echo "$(date) - waiting for db to start"
+#   sleep 1
+# done
+# exec "$@"
+# # npx npx prisma migrate dev --name init
+# # npx prisma db push
+# npx prisma db reset
+# npx prisma db pull
+# npx prisma migrate dev --schema=./prisma/schema.prisma
+# npx prisma db pull
+# # npx prisma db seed
+# # (npx prisma studio&) 
+# # npm run start:dev
