6b8805b5c540c0e9a26101735ad8385e9f967c62
apayetRun <alex-rdc@live.fr>
2023-10-30 10:45:45 +0100

    WIP

--- front/app/src/Component/ConnectionState/index.tsx
+++ front/app/src/Component/ConnectionState/index.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable curly */
 /* eslint-disable max-statements */
 import DisplayConnectionState from "./DisplayConnectionState";
 import { useAppSelector } from "../../Redux/hooks/redux-hooks";
 import DisplayAnonymousConnection from "./DisplayAnonymousConnection";
+import { useEffect, useState } from "react";
+import ServiceChecker from "./ServiceChecker";
+import SessionUserVerificator from "./SessionUserVerificator";
+import AnonymmousUserSessionVerificator
+	from "./AnonymousUserSessionVerificator";
 // import DisplayAnonymousConnect from "./DisplayAnonymousConnect";
 
 const	ConnectionState = () =>
@@ -11,13 +18,59 @@ const	ConnectionState = () =>
 		return (state.server);
 	});
 
-	const	renderConnectState = <DisplayConnectionState />;
-	const	renderAnonymousConnect = <DisplayAnonymousConnection />;
+	const	user = useAppSelector((state) =>
+	{
+		return (state.controller.user);
+	});
 
-	if (server.connexionEnabled === false)
-		return (renderConnectState);
-	else
-		return (renderAnonymousConnect);
+	const
+	[
+		render,
+		setRender
+	]	= useState(<></>);
+
+	useEffect(() =>
+	{
+		// check connection to server
+		if (server.connexionEnabled === false)
+		{
+			setTimeout(() =>
+			{
+				setRender(<ServiceChecker />);
+			}, 200);
+		}
+		else
+		{
+			// timemout for display info
+			if (user.isLoggedIn && user.registrationProcess === false)
+			{
+				setTimeout(() =>
+				{
+					setRender(<SessionUserVerificator />);
+				}, 1000);
+			}
+			else
+			{
+				setTimeout(() =>
+				{
+					setRender(<AnonymmousUserSessionVerificator />);
+				}, 1000);
+			}
+		}
+	},
+	[
+		render,
+		server.connexionEnabled,
+		user.isLoggedIn
+	]);
+	// const	renderConnectState = <DisplayConnectionState />;
+	// const	renderAnonymousConnect = <DisplayAnonymousConnection />;
+
+	// if (server.connexionEnabled === false)
+	// 	return (renderConnectState);
+	// else
+	// 	return (renderAnonymousConnect);
+	return (render);
 };
 
 export default ConnectionState;
